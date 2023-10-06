1235c408a90dccc5063a818e2cd48dfbc87c7790
Bareknuckles <dashkwesi@icloud.com>
2023-10-06 13:10:36 +0100

    Navigation bar feature setup completed

---
+++ imprexions-app/src/components/Navbar.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "./../components/Navbar.scss";
+import logo from "./../assets/logo.jpeg";
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    const navbarMenu = [
+        {
+            name: "About Us",
+            path: "/about"
+        },
+        {
+            name: "New Patient",
+            path: "/newpatient"
+        },
+        {
+            name: "Our Services",
+            path: "/services"
+        },
+        {
+            name: "Contact",
+            path: "/contact"
+        },
+    ]
+    return ( 
+        <div className="main-nav">
+            <div className="container">
+                <nav className="navbar navbar-expand-lg">
+                    <div className="container-fluid">
+                        {/* APP LOGO */}
+                        <Link className="navbar-brand" to="/">
+                            <img src={logo} alt="logo" />
+                        </Link>
+                        <button></button>
+                        <div className="collapse navbar-collapse">
+                            {/* NAVBAR LINKS */}
+                            <ul className="navbar-nav">
+                                {
+                                    navbarMenu.map(navItem => 
+                                        <li className="nav-item">
+                                            <Link className="nav-link" to={navItem.path}>{navItem.name}</Link>
+                                        </li>    
+                                    )
+                                }
+                            </ul>
+                            <div className="theme-btn">
+                                <Link to="/">Book Appointment</Link>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+     );
+}
+ 
+export default Navbar;
